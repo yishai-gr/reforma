b5315edbbe33901c886bf2f15e6f8d94d8f324bb
Yishai <yishai5765@gmail.com>
2023-07-14 15:25:13 +0300

    fix skeleton

--- basic.js
+++ basic.js
@@ -1,10 +1,14 @@
+var head = document.getElementsByTagName("head")[0];
+var link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "/basic.css";
+head.appendChild(link);
 async function load_nav_and_footer() {
   const nav = document.createElement("div");
-  nav.innerHTML = await (await fetch("../nav and footer/nav.html")).text();
+  nav.innerHTML = await (await fetch("/nav and footer/nav.html")).text();
   const footer = document.createElement("div");
-  footer.innerHTML = await (
-    await fetch("../nav and footer/footer.html")
-  ).text();
+  footer.innerHTML = await (await fetch("/nav and footer/footer.html")).text();
   document.body.prepend(nav);
   document.body.append(footer);
 }
